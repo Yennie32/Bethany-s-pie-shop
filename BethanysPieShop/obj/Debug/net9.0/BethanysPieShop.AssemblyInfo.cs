3a680f62918777f35d49890cd600da4f6daae992
Yennie <yennielake@gmail.com>
2025-02-04 11:35:35 +0100

    tag helpers added from pie/list page to detail page

--- BethanysPieShop/obj/Debug/net9.0/BethanysPieShop.AssemblyInfo.cs
+++ BethanysPieShop/obj/Debug/net9.0/BethanysPieShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86b6e3de4dbbb09276645bee35c1de5497d10988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802cd634cc3259348f52a06810642ef393dc6a03")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
