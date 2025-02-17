3af96a557084f74c12df9f96f0ebe50c5c8bc400
Yennie <yennielake@gmail.com>
2025-02-17 11:37:09 +0100

    API created

---
+++ BethanysPieShop/Controllers/Api/SearchController.cs
@@ -0,0 +1,33 @@
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BethanysPieShop.Controllers.Api
+{
+    // indicates a route to route requests to this controller. 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly IPieRepository _pieRepository;
+        public SearchController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allPies = _pieRepository.AllPies;
+            return Ok(allPies);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            if (!_pieRepository.AllPies.Any(p => p.PieId == id))
+                return NotFound();
+
+            return Ok(_pieRepository.AllPies.Where(p => p.PieId == id));
+        }
+    }
+}
